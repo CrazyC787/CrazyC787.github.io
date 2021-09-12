2e325561b43f408f7bb4320dc42dde0e18d8725b
CrazyC787 <CrazyC787@GMAIL.COM>
2021-09-12 06:14:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -30,11 +30,11 @@ let lastID;
             break;
           
           case "discord":
-            text = "My Discord is <code>jad#9938</code>";
+            text = "My Discord is <code>Brazy#0501</code>";
             break;
           
           case "gmail":
-            text = "My Email is <code>jadchehimi0@gmail.com</code><br>Please don't spam it.";
+            text = "My Email is <code>CrazyC787@gmail.com</code><br>Please don't spam it.";
             break;
         }
 
