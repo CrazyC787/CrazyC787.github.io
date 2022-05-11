fad66514d40e154dd8042edbaaa70f87ff8a2a0b
Brazman <CrazyC787@GMAIL.COM>
2022-05-11 05:42:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ let lastID;
             break;
           
           case "discord":
-            text = "My Discord is <code>jad#0024</code>";
+            text = "My Discord is <code>Brazy#0501</code>";
             break;
         }
 
